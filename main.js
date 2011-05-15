7adcc7b366a4a6e424c46085dfdb5dfb3b6cb7f4
Eliseo Soto <eliseo_soto@apple.com>
2011-05-15 02:16:11 -0700

    cleanup done, need to calculate footer results

--- main.js
+++ main.js
@@ -1,42 +1,72 @@
-//all the jsonp magic is here, we create an script element and we set the url and callback function.
+refreshRate = 5000; // In Milliseconds
+
+/**
+Inserts <script> in the <head> to handle JSONP requests
+**/
 function jsonp(url, callback) {
     var head = document.getElementsByTagName("head")[0];
+    var scripts = head.getElementsByTagName("script");
+    var scriptToRemove;
     
-    // Create wrapper function
+    // Remove previous wrapper function
+    var wrapScriptPrefix = callback + "_";
+    for(i in scripts) {
+        if(scripts[i].type == "text/javascript" && scripts[i].innerHTML && beginsWith(scripts[i].innerHTML, wrapScriptPrefix)) {
+            scriptToRemove = scripts[i];
+            break;
+        } 
+    }
+    if(scriptToRemove) {
+        head.removeChild(scriptToRemove);
+        scriptToRemove = null;
+    }
+    
+    // Create wrapper function that points to the callback
     var wrapScript = document.createElement("script");
     wrapScript.type = "text/javascript";
-    callbackWrap = callback + "_" + Math.round(Math.random() * 10000);
+    callbackWrap = wrapScriptPrefix + Math.round(Math.random() * 10000); // Adds a _9999 suffix
     wrapScript.innerHTML = callbackWrap + '=' + callback ;
     head.appendChild(wrapScript);
+    
+    // Remove previous script
+    for(i in scripts) {
+        if(scripts[i].type == "text/javascript" && scripts[i].src && beginsWith(scripts[i].src, url)) {
+            scriptToRemove = scripts[i];
+            break;
+        }
+    }
+    if(scriptToRemove) {
+        head.removeChild(scriptToRemove);
+    }
     
     var scriptElement = document.createElement("script");
     scriptElement.type = "text/javascript";
     // i add to the url the call back function
     var jsonpReq = url + "&jsoncallback=" + callbackWrap;
     scriptElement.src = jsonpReq;
-    
-    // If found then remove it
-    var scripts = head.getElementsByTagName("script");
-    var scriptToRemove;
-    for(i in scripts) {
-        if(scripts[i].type == "text/javascript" && scripts[i].src == jsonpReq) {
-            scriptToRemove = scripts[i];
-        }
-    }
-    if(scriptToRemove) {
-        head.removeChild(scriptToRemove);
-    }
     head.appendChild(scriptElement);
 }
 
-//this function just set the url, and make the call
+/**
+Determines if string begins with subString
+**/
+function beginsWith(string, subString) {
+    return string.substr(0, subString.length) === subString;
+}
+
+/**
+Main function that gets refreshed every refreshRate milliseconds and makes the call to the SFPark service
+**/
 function getPark() {
     var url = "http://api.sfpark.org/sfpark/rest/availabilityservice?lat=37.792275&long=-122.397089&radius=2&uom=mile&response=json";
     jsonp(url, 'sfParkResults');
     
-    setTimeout("getPark()", 5000);
+    setTimeout("getPark()", refreshRate);
 }
 
+/**
+Creates a <tr> with a variable number of <td>s
+**/
 function createTableRow(element) {
     var args = arguments;
     var tableRow = document.createElement('tr');
@@ -50,16 +80,19 @@ function createTableRow(element) {
     return tableRow;
 }
 
+/**
+Refreshes the UI with the data coming from the SFPark service
+**/
 function sfParkResults(parking) {    
     var lastUpdated = document.getElementById('lastUpdated');
     var table = document.getElementById('parkResults');
     
-    lastUpdated.innerHTML = parking.AVAILABILITY_UPDATED_TIMESTAMP + Math.random();
+    lastUpdated.innerHTML = parking.AVAILABILITY_UPDATED_TIMESTAMP;
     
     // Get the table
     var tbody = table.getElementsByTagName("tbody")[0];
     
-    // Remove all the table contents
+    // Remove the table contents
     if(tbody.hasChildNodes()) {
         while(tbody.childNodes.length >= 1) {
             tbody.removeChild(tbody.firstChild);
@@ -75,6 +108,11 @@ function sfParkResults(parking) {
         var tableRow = createTableRow(obj.NAME, obj.OPER, obj.OCC, percentage);
         tbody.appendChild(tableRow);
     }
+    
+    // Footer
+    var tfoot = table.getElementsByTagName("tfoot")[0];
+    tfoot.removeChild(tfoot.firstChild);
+    tfoot.appendChild(createTableRow(1,2,3,Math.random()));
 }
 
 // wait for all the page elements to load (including images), not very effective but I wanted to keep it simple
